dea9b4950bc06debf5d891d188b9729c01a5e8ef
Julia Rzymowska <jrzymowska@student.agh.edu.pl>
2024-11-04 13:37:02 +0100

    is_prime bug fix and base_convertion update

--- base_convertion.py
+++ base_convertion.py
@@ -1,7 +1,42 @@
-def base_convertion(number : int, base : int):
+from math import log10
+
+def base_convertion_1(number : int, base : int):
+    ans = ''
+    cnt = 0
+    hex = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
+
+    while number > 0:
+        ans += hex[number%base]
+        number //= base
+
+    b = ''
+    for i in range(len(ans)):
+        b += ans[len(ans) - i - 1]
+
+    return b
+
+def base_convertion_string(number : int, base : int):
     if number > 0:
-        base_convertion(number // base, base)
+        base_convertion_string(number // base, base)
         print('0123456789ABCDEFGHIJKLMNOPQRSTUWXYZ'[number%base], end = '')
+
+def base_convertion_list(number : int, base : int):
+    l = int( log10(number) ) + 1
+    t = [-1 for _ in range(l)]
+    cnt = 0
+    hex = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
+
+
+    while number > 0:
+        t[cnt] = number%base
+        cnt += 1
+        number //= base
+
+    cnt -= 1
+
+    while cnt>=0:
+        print(hex[t[cnt]], end = '')
+        cnt -= 1
 
 def main():
     n = int(input()) # number
@@ -10,6 +45,9 @@ def main():
     if n == 0:
         print(0)
     else:
-        base_convertion(n,b)
+        base_convertion_string(n,b)
+        print('\n')
+        print(base_convertion_1(n, b))
+        base_convertion_list(n, b)
 
 main()
--- is_prime.py
+++ is_prime.py
@@ -1,10 +1,11 @@
 def is_prime(number : int) -> int:
-    if number <= 2:
-        return True
+    if number < 2:
+        return False
     i = 2
     while i*i <= number:
         if number % i == 0:
             return False
+        i += 1
     #end while
 
     return True
