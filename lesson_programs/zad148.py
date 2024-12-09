a3a3ba397068b0c6f1913995f44b05042308f37c
Julia Rzymowska <jrzymowska@student.agh.edu.pl>
2024-12-09 17:00:39 +0100

    remove .idea, python_learning programs, test.py and zad148

---
+++ lesson_programs/zad148.py
@@ -0,0 +1,17 @@
+def hetman(t: list, x=0, p1=[], p2=[], k=[]):
+    if x == 8:
+        print(t)
+        exit()
+
+    for i in range(8):
+        if check(x, i, p1, p2, k):
+            t[x] = i
+            k[i] = True
+            p1[x + 1] = True
+            p2[x - i] = True
+            hetman(t, x + 1, p1, p2, k) # jeśli dla wszystkich kombinacji return
+            k[i] = p1[x + i] = p2[x - i] = False
+
+
+def check(x, y, p1, p2, k):
+    return p1[x + y] == False and k[y] == False and p2[x - y] == False
